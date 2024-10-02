bec988a2f0650e3e40e7a31c5f19807a24dc4124
Shiki <victorio1delgado@gmail.com>
2024-10-01 19:23:27 -0800

    feat() game paper rock scissors

--- main.py
+++ main.py
@@ -1,23 +1,60 @@
 import random
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-pass_letter =[]
-pass_symbol =[]
-pass_number =[]
-print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
-for letter in range(0,nr_letters):
-    pass_letter.append(random.choice(letters))
-for symbol in range(0,nr_symbols):
-    pass_symbol.append(random.choice(symbols))
-for number in range(0,nr_numbers):
-    pass_number.append(random.choice(numbers))
-password = pass_letter + pass_symbol + pass_number
-random.shuffle(password)
-password = ''.join(password)
-print(f"Your password is: {password}")
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
 
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
 
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+options = [rock,paper,scissors]
+valid = False
+random_choice =random.randint(0,2)
+option = int(input( " what do you choose type 0 for rock 1 for paper 2 for scissors "))
+if option == 0:
+    print(rock)
+    if (options[random_choice]== scissors):
+        valid = True
+    elif (options[random_choice]== paper):
+        valid = False
+elif option == 1:
+    print(paper)
+    if (options[random_choice]== rock):
+        valid = True
+    elif (options[random_choice]== scissors):
+        valid = False
+elif option == 2:
+    print(scissors)
+    if (options[random_choice]== paper):
+        valid = True
+    elif (options[random_choice]== rock):
+        valid = False
+else:
+    print("invalid option " + str(option))
+print("computer choose" )
+print(options[random_choice])
+
+if(random_choice == option):
+    print("a draw")
+elif(valid == True):
+    print("you win")
+elif(valid == False):
+    print("you lose")
